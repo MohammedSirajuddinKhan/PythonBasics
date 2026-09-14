65877d2873ee08a264bf104042d5574db06b0d02
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-09-14 12:00:21 +0530

    Sets are Mutable

---
+++ sets.py
@@ -0,0 +1,8 @@
+# This stores unique values and is unordered
+
+sports = {"mma","karate","judo","jiu jitsu","boxing","mma","karate"}
+print(type(sports))
+print(set(sports)) # This will remove duplicates and print unique values
+print(sports) # This will print the original set with duplicates
+
+# VENN DIAGRAM IS A MUST FOR THIS BEAUTY
